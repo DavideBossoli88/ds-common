b64dd366f1158a4dd1175a6216828467eaf603a1
DavideBossoli88 <davidebossoli@libero.it>
2022-04-27 14:54:27 +0200

    Added .py

--- setup_utils/docker_containers/container_processing_sagemaker/build.sh
+++ setup_utils/docker_containers/container_processing_sagemaker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t ds-common-0 --build-arg username_proxy=null --build-arg password_proxy=null --build-arg git_token=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 .
+docker build -t train_automl --build-arg username_proxy=null --build-arg password_proxy=null --build-arg git_token=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 .
